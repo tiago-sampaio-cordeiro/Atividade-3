1e9d169a85fe928dc132ef6afab91d832eddf351
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2024-11-03 00:35:18 -0300

    pequena correção no estilo

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -6,22 +6,22 @@ export default function Login() {
   const [Password, setPassword] = useState("");
 
   return (
-    <View>
+    <View style = {{width:200}}>
       <Text>Username:</Text>
       <TextInput
-        style={{ padding: 1, borderWidth: 1, borderColor: "blue" }}
+        style={{ padding: 1, borderWidth: 1,borderRadius:5, borderColor: "blue" }}
         value={Username}
         onChangeText={(text) => setName(text)}
       />
       <Text>Password:</Text>
       <TextInput
-        style={{ padding: 1, borderWidth: 1, borderColor: "blue" }}
+        style={{ marginBottom:50, padding: 1, borderWidth: 1, borderColor: "blue" }}
         value={Password}
         onChangeText={(text) => setPassword(text)}
         secureTextEntry={true}
       />
       <Button
-        title="Press me"
+        title="Logar"
         onPress={() => console.log('O Usuário é: ' + Username + ' ' + 'e a senha é: ' + Password)}
       />
     </View>
